bc282752ec16b90d479d9484f3b8b74f87c1e471
Zachary Danger Campbell <zacharydangercampbell@gmail.com>
2014-04-30 21:09:24 -0500

    Use the rvm wrapper for god

--- manual_setup/config_files/runit/god-run-script.sh
+++ manual_setup/config_files/runit/god-run-script.sh
@@ -3,4 +3,4 @@ exec 2>&1
 
 trap 'kill -HUP %1' 1 2 13 15
 
-god --no-daemonize --config-file /etc/god/master.god --no-syslog & wait
+/var/god/.rvm/bin/god --no-daemonize --config-file /etc/god/master.god --no-syslog & wait
